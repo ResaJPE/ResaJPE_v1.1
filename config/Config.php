0f802fc2a6ee8ce1cd03c83c7c4a04a5dec9f6c6
FabienBdn <46462355+FabienBdn@users.noreply.github.com>
2019-01-08 17:11:03 +0100

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -3,12 +3,12 @@ class Config {
 	// Paramètres pour l'accès à la base de données
 	const BDD_HOST      = 'localhost';
 	const BDD_NAME      = 'JPE_v0';
-	const BDD_USER      = 'usersio';
-	const BDD_PASSWORD  = 'pwsio';
+	const BDD_USER      = 'userweb';
+	const BDD_PASSWORD  = 'pwweb';
 
 	// Paramètres pour l'application
 	const APPLI_NAME    = 'Resa-JPE';
 	const APPLI_NAME_SHORT = 'JPE';
-	const APPLI_VERSION = '0.1';
+	const APPLI_VERSION = '1';
 }
 
