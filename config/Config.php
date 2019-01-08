992a82a7de1803e03c97d9d1679baf5bfdfa7c30
FabienBdn <46462355+FabienBdn@users.noreply.github.com>
2019-01-08 17:11:15 +0100

    Update Config.php

--- config/Config.php
+++ config/Config.php
@@ -2,7 +2,7 @@
 class Config {
 	// Paramètres pour l'accès à la base de données
 	const BDD_HOST      = 'localhost';
-	const BDD_NAME      = 'JPE_v0';
+	const BDD_NAME      = 'JPE_v1';
 	const BDD_USER      = 'userweb';
 	const BDD_PASSWORD  = 'pwweb';
 
